91875f4f1b04c77d756ef3616fa544b9b09a789f
toan.pham <toan.pham@weddingbook.com>
2021-12-04 13:48:33 +0700

    phan quyen cho front end

--- src/main/java/com/solienlac/khoaluan/web/service/impl/SinhVienServiceImpl.java
+++ src/main/java/com/solienlac/khoaluan/web/service/impl/SinhVienServiceImpl.java
@@ -1,5 +1,8 @@
 package com.solienlac.khoaluan.web.service.impl;
 
+import com.solienlac.khoaluan.web.common.dto.ThongTinSinhVienDto;
+import com.solienlac.khoaluan.web.domain.SinhVien;
+import com.solienlac.khoaluan.web.repository.SinhVienRepository;
 import com.solienlac.khoaluan.web.service.SinhVienService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,4 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class SinhVienServiceImpl implements SinhVienService {
+    private final SinhVienRepository sinhVienRepository;
+    @Override
+    public ThongTinSinhVienDto xemThongTin(Integer id) {
+        SinhVien sinhVien = sinhVienRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("id not found"));
+
+        return new ThongTinSinhVienDto(sinhVien);
+    }
 }
